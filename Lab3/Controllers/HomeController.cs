5dc0e805daa81e001282bcfa8e18bfa82d8df898
Yuso0001 <yuso0001@algonquinlive.com>
2023-06-12 19:28:07 -0400

    Initial and Final

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing() {
+            // you will complete this
+            HttpContext.Session.SetString("num", Request.Form["num"]);
+            return View();
+        }
+
+        public IActionResult CreateStudent() => View();
+
+        [HttpPost]
+        public IActionResult DisplayStudent(Student student) {
+            // you will complete this
+            return View(student);
+        }
+        public IActionResult Error() {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab3.Models {
+    public class Student {
+        public int StudentId { get; set; }
+        public string FirstName { get; set;}
+        public string LastName { get; set;}
+        public string Email { get; set;}
+        public string Password { get; set;}
+        public string StudentDescription { get; set;}
+    }
+}
