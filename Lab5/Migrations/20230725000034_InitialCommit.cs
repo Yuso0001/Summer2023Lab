8f9c80906be25746e924b1e28c70915d81734e71
Yuso0001 <yuso0001@algonquinlive.com>
2023-07-24 20:02:09 -0400

    Initial Commit

---
+++ Lab5/Migrations/20230725000034_InitialCommit.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab5.Migrations
+{
+    public partial class InitialCommit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Prediction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FileName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    URL = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Question = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prediction", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Prediction");
+        }
+    }
+}
---
+++ Lab5/Models/Prediction.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab5.Models {
+    public enum Question {
+        Earth, Computer
+    }
+    public class Prediction {
+
+        [Required]
+        [Column("Id")]
+        public int PredictionId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Column("FileName")]
+        public string FileName { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        [Column("URL")]
+        public string URL { get; set; }
+
+        [Required]
+        [Column("Question")]
+        public Question Question { get; set; }
+    }
+}
